b653f24d816d7c99c4df83af906fabb0c6a2914c
David Jimbo <david.jimbo26@gmail.com>
2015-01-07 00:16:21 -0500

    Website

---
+++ phpex/cart.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+	<html>
+		<head> 
+			<title> FU </title>
+			<link rel="stylesheet" href="css/style.css">
+		</head>
+			<body>
+				<header> 
+					<a href ="i.html"><H1> Games R' Us</H1> </a>
+					
+					<div class = "wrapper">
+						<nav>
+							<ul>
+								<li>
+									<a href ="ps4stock.php">
+										<p> PS4 </p>
+										<img src = "images/header/ps4.jpg" alt = "PS4" /> 
+									</a>
+									
+								</li>
+								<li>
+									<a href ="ps4stock.php">
+										<p> XBOX ONE </p>
+										<img src = "images/header/xboxone.jpg" alt = "Xbox One" />
+									</a>
+									
+								</li>
+								<li>
+									<a href ="ps4stock.php">
+										<p> WII U </p>
+										<img src = "images/header/wiiu.jpg" alt = "Wii U" />
+									</a>
+									
+								</li>
+								<li>
+									<a href ="ps4stock.php">
+										<p> CONTACT </p>
+										<img src = "images/header/contact.jpg" alt = "Contact" />
+									</a>
+									
+								</li>
+								<li>
+									<a href ="ps4stock.php">
+										<p> CART </p>
+										<img src = "images/header/cart.jpg" alt = "Cart" />
+									</a>
+								</li>
+							</ul>
+							
+						</nav>	
+					</div>
+				
+				</header>
+				
+				<div class = "banner">
+					<div class = "main">
+						<img src = "images/banner/joker.jpg" alt = "Joker"/>
+					</div>
+					<div class= "side">
+						<H1> New Releases </H1>
+ 					</div>
+					<div class = "sideimgs"> 
+						<img src = "images/ps4/fifa2015.jpe" alt = "Advance Warefare" />
+						
+						<img src = "images/xbox/aw.jpe" alt = "Advance Warefare" />
+						
+						<img src = "images/wiiu/smash.jpe" alt = "Advance Warefare" />
+
+						
+						
+					</div>
+					<H3> There will be new releases every Monday of the latest games so sighnup today </H3>
+				</div>
+				
+				<footer>
+					David Jimbo copyright &copy <?php echo date("Y") ?>
+				</footer>
+				
+				
+				
+			</body> 
+		</div>
+		
+	</html>
